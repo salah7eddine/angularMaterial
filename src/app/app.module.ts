706f88791d8ca41fadb442580124cdf8667df83d
Abousaid Salah-Eddine <abousaid7salah@gmail.com>
2021-06-01 15:41:58 +0100

    Responsive Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,15 @@ import { DataTableComponent } from './data-table/data-table.component';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
+import { MainNavComponent } from './main-nav/main-nav.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, DataTableComponent],
+  declarations: [AppComponent, DataTableComponent, MainNavComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -20,6 +26,11 @@ import { MatSortModule } from '@angular/material/sort';
     MatTableModule,
     MatPaginatorModule,
     MatSortModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
